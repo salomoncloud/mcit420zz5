7afe76c52d9c9b96ef6baecf9db19ed6313c3271
Sami Islam <islam.samipkr@gmail.com>
2024-01-09 08:58:58 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -7,7 +7,7 @@ locals{
   total_output = ["150", "150", "150"]
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
- character_enemy_map =   { for character in local.characters: # Convert character list to a set
-      character => local.enemies_destroyed
+ character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed[index]
 }
 }
