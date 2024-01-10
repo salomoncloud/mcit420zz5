774286302052a5fc849c0c527d94d49656de3eb2
Sami Islam <islam.samipkr@gmail.com>
2024-01-10 14:33:43 -0500

    Create readyaml_linuxapp.tf

---
+++ readyaml_linuxapp.tf
@@ -0,0 +1,11 @@
+locals{
+  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
