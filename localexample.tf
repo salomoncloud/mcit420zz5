55c19f97325378de5a0600c14535577dbcdc9018
Sami Islam <islam.samipkr@gmail.com>
2024-01-10 12:40:27 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -7,7 +7,33 @@ locals{
   total_output = ["150", "150", "150"]
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
- character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed[index]
+  }
+   
 }
+variable "nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
+}
+
+locals {
+  flattened_list = flatten(var.nested_list)
+  flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }])
+}
+
+output "flattened_list_result" {
+  value = local.flattened_list
+}
+
+output "flattened_map_result" {
+  value = local.flattened_map
 }
