69697d5c3c217366b11f73fa7f4282c88102d0d5
Sami Islam <islam.samipkr@gmail.com>
2024-01-10 14:34:26 -0500

    Update readyaml_linuxapp.tf

--- readyaml_linuxapp.tf
+++ readyaml_linuxapp.tf
@@ -9,3 +9,21 @@ locals{
       }
     ]
 ])
+resource "azurerm_service_plan" "batcha06sp" {
+  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
+
+resource "azurerm_linux_web_app" "batcha06webapp" {
+  for_each            = azurerm_service_plan.batcha06sp
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  service_plan_id     = each.value.id
+
+  site_config {}
+}
