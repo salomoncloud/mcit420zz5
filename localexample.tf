d79bf3e6c50ea108dfa911a3044958442f31ce92
Sami Islam <islam.samipkr@gmail.com>
2024-01-10 12:42:55 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -1,17 +1,3 @@
-locals{
-  servicename="abracadabra"
-  forum="abracadabramcit"
-  lengthsa=length(local.servicename)
-  lengthforum=length(local.forum)
-  winterlistOfSports=["icehockey","snowboarding","iceskating"]
-  total_output = ["150", "150", "150"]
-  characters = ["luke", "yoda", "darth"]
-  enemies_destroyed = [4252, 900, 20000056894]
-  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
-      character => local.enemies_destroyed[index]
-  }
-   
-}
 variable "nested_list" {
   type    = list(list(string))
   default = [["a", "b"], ["c", "d"], ["e", "f"]]
@@ -25,10 +11,23 @@ variable "nested_map" {
   }
 }
 
-locals {
+locals{
+  servicename="abracadabra"
+  forum="abracadabramcit"
+  lengthsa=length(local.servicename)
+  lengthforum=length(local.forum)
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["150", "150", "150"]
+  characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed[index]
+  }
   flattened_list = flatten(var.nested_list)
   flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
+   
 }
+
 
 output "flattened_list_result" {
   value = local.flattened_list
