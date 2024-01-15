0aa42b0065f2f19ed54664268ad8380e0cdfb435
Sami Islam <islam.samipkr@gmail.com>
2024-01-15 18:56:48 -0500

    Update wafpolicy.tf

--- wafpolicy.tf
+++ wafpolicy.tf
@@ -2,7 +2,7 @@ locals{
   waf_policy=[for f in fileset("${path.module}/waffolder", "[^_]*.yaml") : yamldecode(file("${path.module}/waffolder/${f}"))]
   azurewafpolicy_list = flatten([
     for app in local.waf_policy: [
-      for azurewaf in try(app.azurewafpolicy, []) :{
+      for azurewaf in try(app.listofwafpolicy, []) :{
         name=azurewaf.name
       }
     ]
