30af6239506e25e069a1bac9bad409acad4ed3f1
Sami Islam <islam.samipkr@gmail.com>
2024-01-15 18:45:41 -0500

    Update wafpolicy.tf

--- wafpolicy.tf
+++ wafpolicy.tf
@@ -1,3 +1,15 @@
+locals{
+  linux_app=[for f in fileset("${path.module}/waffolder", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
+}
 resource "azurerm_resource_group" "example" {
   name     = "example-rg"
   location = "West Europe"
