522687c49bd85a4cb414cf4f93c91f55c83879ca
Sami Islam <islam.samipkr@gmail.com>
2024-05-01 16:16:28 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -62,37 +62,4 @@ variable "summersports"{
     type =list(string)
     default=["soccer","basketball","badminton","tabletennis"]
 }
-//==================================================1st May 2024=========================================//
-variable "original_string" {
-  default = "Montreal College Information Technology"
-}
-
-# Function 1: Length of the string
-output "string_length" {
-  value = length(var.original_string)
-}
-
-# Function 2: Uppercase the string
-output "uppercase_string" {
-  value = upper(var.original_string)
-}
-
-# Function 3: Lowercase the string
-output "lowercase_string" {
-  value = lower(var.original_string)
-}
-
-# Function 4: Replace "Hello" with "Hi"
-output "replaced_string" {
-  value = replace(var.original_string, "Montreal College Information Technology", "McGill University")
-}
-
-# Function 5: Concatenate with another string
-variable "additional_string" {
-  default = " from Terraform"
-}
-
-output "concatenated_string" {
-  value = "${var.original_string}${var.additional_string}"
-}
 
