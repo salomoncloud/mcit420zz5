91810cdf75c930633de4400771399f92885e6e4b
Sami Islam <islam.samipkr@gmail.com>
2024-05-03 19:44:26 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -22,11 +22,13 @@ locals{
   lengthforum=length(local.forum)
   winterlistOfSports=["icehockey","snowboarding","iceskating"]
   total_output = ["150", "150", "150"]
+//==============================================Map example start===============================
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
   character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed[index]
   }
+//=============================================Map example end===============================
   simple_local_flattened_list = flatten(var.simple_nested_list)
   flattened_list = flatten(var.nested_list)
   flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
